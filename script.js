075b973736f73eda08df3bffc09be807516e9d93
Stijn Claeys <atcready@outlook.com>
2025-07-23 14:59:10 +0200

    functionality: added first javascript

---
+++ script.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.querySelector('.btn');
+    const startScreen = document.querySelector('.start-screen');
+    const exerciseScreen = document.querySelector('.exercise-screen');
+
+    startButton.addEventListener('click', () => {
+        startScreen.classList.add('hidden');
+        exerciseScreen.classList.remove('hidden');
+    });
+});
+
+
+function clockToCoordinates(clock, distanceNM, centerX, centerY, maxDistanceNM = 15, radius = 185) {
+    const angleDeg = ((clock % 12) * 30) - 90; // 12 = 0°, 3 = 90°, etc.
+    const angleRad = angleDeg * (Math.PI / 180);
+    const scale = radius / maxDistanceNM;
+
+    const x = centerX + distanceNM * scale * Math.cos(angleRad);
+    const y = centerY + distanceNM * scale * Math.sin(angleRad);
+    return { x, y };
+}
+
+const center = 150;
+const { x, y } = clockToCoordinates(11, 4, center, center);
+centerIntX = x;
+centerIntY = y;
+const headingInt = scenarios.intruder.heading;
+const headingTgt = scenarios.target.heading;
+const intruderHdg = clockToCoordinates(headingInt / 30, 2, centerIntX, centerIntY);
+const targetHdg = clockToCoordinates(headingTgt / 30, 2, center, center);
+
+function renderIntruder(x, y, callsign, type, level) {
+    const svg = document.querySelector(".radar-screen svg");
+
+    // Aircraft square (intruder)
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("x", x - 5);
+    rect.setAttribute("y", y - 5);
+    rect.setAttribute("width", "10");
+    rect.setAttribute("height", "10");
+    rect.setAttribute("fill", "#FFF");
+    svg.appendChild(rect);
+
+
+    // Leader line (intruder)
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", x);
+    line.setAttribute("y1", y);
+    line.setAttribute("x2", intruderHdg.x);
+    line.setAttribute("y2", intruderHdg.y);
+    line.setAttribute("stroke", "#FFF");
+    line.setAttribute("stroke-width", "1");
+    svg.appendChild(line);
+}
+
+function renderTgt(x, y, callsign, type, level) {
+    const svg = document.querySelector(".radar-screen svg");
+
+    // Aircraft square (target)
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("x", center - 5);
+    rect.setAttribute("y", center - 5);
+    rect.setAttribute("width", "10");
+    rect.setAttribute("height", "10");
+    rect.setAttribute("fill", "#FFF");
+    svg.appendChild(rect);
+
+
+    // Leader line (target)
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", center);
+    line.setAttribute("y1", center);
+    line.setAttribute("x2", targetHdg.x);
+    line.setAttribute("y2", targetHdg.y);
+    line.setAttribute("stroke", "#FFF");
+    line.setAttribute("stroke-width", "1");
+    svg.appendChild(line);
+}
+
+renderIntruder(x, y)
+renderTgt(x, y)
